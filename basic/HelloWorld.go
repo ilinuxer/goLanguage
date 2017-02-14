716abc7b8babc65e04c185dd378c2856dd2f2ab1
zhzhh <zhihongzhang_hi@163.com>
2017-02-14 22:23:29 +0800

    goLang hello world

---
+++ basic/HelloWorld.go
@@ -0,0 +1,25 @@
+package main
+
+import . "fmt"
+const const_var = 1
+
+//小写为private
+var var_val = 2;
+
+//大写为public
+var Var_val = 3;
+
+type IntType  int
+
+type StructType struct{}
+
+type Interface interface {
+
+}
+
+func main(){
+  Println("Hello World")
+}
+
+
+
